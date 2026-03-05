ed06e8a88e3c42fe24872b130a827e216dd5aafc
Evelinenkyang <20253rd059@utez.edu.mx>
2026-03-05 16:06:38 -0600

    Termine el main

--- Main.java
+++ Main.java
@@ -1,13 +1,23 @@
 public class Main{
     public static void main(String[]args){
 
-        Alumno juan = new Alumno();
+        Alumno  = new Alumno();
         String nombre = pd.pedircadna("Nombre del alumno");
-        int matricula =pd.pedircadna("Matricula");
-        double calificacion1=pd.pedircadna("Calificacion 1",0,10);
-        double calificacion2=pd.pedircadna("Calificacion 2",0,10);
-        double calificacion3=pd.pedircadna("Calificacion 3",0,10);
-        double promedio=pd.pedircadna("Promedio final");
+        int matricula = pd.pedircadna("Matricula");
+        double calificacion1= pd.pedircadna("Calificacion 1",0,10);
+        double calificacion2 = pd.pedircadna("Calificacion 2",0,10);
+        double calificacion3 = pd.pedircadna("Calificacion 3",0,10);
+        double promedio = pd.pedircadna("Promedio final");
+
+        Alumno a= new Alumno (nombre,matricula,calificacion1,calificacion2,calificacion3,promedio);
+
+        Escuela esc = new Escuela(a);
+        System out printnl("---Informacion del alumno---");
+        esc.mostrarAlumno();
+    
+
+
+
 
         
 
