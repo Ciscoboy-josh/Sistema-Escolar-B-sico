c5fe0059a131a4c74fb10755f1e50e6b6e75c5f1
Evelinenkyang <20253rd059@utez.edu.mx>
2026-03-05 16:01:12 -0600

    añadi los atributos

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
 public class Main{
     public static void main(String[]args){
 
+        Alumno juan = new Alumno();
+        String nombre = pd.pedircadna("Nombre del alumno");
+        int matricula =pd.pedircadna("Matricula");
+        double calificacion1=pd.pedircadna("Calificacion 1",0,10);
+        double calificacion2=pd.pedircadna("Calificacion 2",0,10);
+        double calificacion3=pd.pedircadna("Calificacion 3",0,10);
+        double promedio=pd.pedircadna("Promedio final");
+
+        
+
+
 
     }
 }
