cbb237e9145e9fe36b77b1d9a0215e1aa28a6255
Evelinenkyang <20253rd059@utez.edu.mx>
2026-03-05 16:40:11 -0600

    cambios

--- Main.java
+++ Main.java
@@ -8,12 +8,12 @@ public class Main{
         double calificacion1= pd.pedirDecimal("Calificacion 1",0,10);
         double calificacion2 = pd.pedirDecimal("Calificacion 2",0,10);
         double calificacion3 = pd.pedirDecimal("Calificacion 3",0,10);
-        double promedio = pd.pedirDecimal("Promedio final");
+        
 
-        Alumno a= new Alumno (nombre,matricula,calificacion1,calificacion2,calificacion3,promedio);
+        Alumno a= new Alumno (nombre,matricula,calificacion1,calificacion2,calificacion3);
 
         Escuela esc = new Escuela(a);
-        System.out.printnl("---Informacion del alumno---");
+        System.out.println("---Informacion del alumno---");
         esc.mostrarAlumno();
     
 
