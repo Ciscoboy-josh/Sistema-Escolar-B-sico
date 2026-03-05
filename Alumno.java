d2bd5a17d1e771fe83a714a708e1adda78a89c49
Ciscoboy-josh <Orochiponce1@gmail.com>
2026-03-05 15:39:05 -0600

    cree las variables y el constructor

---
+++ Alumno.java
@@ -0,0 +1,26 @@
+public class Alumno{
+    String nombre;
+    int matricula;
+    double calificacion1;
+    double calificacion2;
+    double calificacion3;
+
+    public Alumnos(String nombre,int matricula,double calificacion1,double calificacion2,double calificacion3){
+    this.nombre = nombre;
+    this.matricula = matricula;
+    this.calificacion1 = calificacion1;
+    this.calificacion2 = calificacion2;
+    this.calificacion3 = calificacion3;
+
+    }
+    public void calcularPromedio()
+
+    
+
+
+
+
+
+
+
+}
