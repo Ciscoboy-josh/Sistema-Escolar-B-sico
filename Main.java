b993e24701efcc37c7590dce37d0e82f7a8aaf27
Evelinenkyang <20253rd059@utez.edu.mx>
2026-03-05 16:34:35 -0600

    hice mas cambios

--- Main.java
+++ Main.java
@@ -1,18 +1,19 @@
 public class Main{
     public static void main(String[]args){
+        Pedir_datos pd= new Pedir_datos();
 
-        Alumno  = new Alumno();
+        Alumno a = new Alumno();
         String nombre = pd.pedircadna("Nombre del alumno");
-        int matricula = pd.pedircadna("Matricula");
-        double calificacion1= pd.pedircadna("Calificacion 1",0,10);
-        double calificacion2 = pd.pedircadna("Calificacion 2",0,10);
-        double calificacion3 = pd.pedircadna("Calificacion 3",0,10);
-        double promedio = pd.pedircadna("Promedio final");
+        int matricula = pd.pedirEntero("Matricula");
+        double calificacion1= pd.pedirDecimal("Calificacion 1",0,10);
+        double calificacion2 = pd.pedirDecimal("Calificacion 2",0,10);
+        double calificacion3 = pd.pedirDecimal("Calificacion 3",0,10);
+        double promedio = pd.pedirDecimal("Promedio final");
 
         Alumno a= new Alumno (nombre,matricula,calificacion1,calificacion2,calificacion3,promedio);
 
         Escuela esc = new Escuela(a);
-        System out printnl("---Informacion del alumno---");
+        System.out.printnl("---Informacion del alumno---");
         esc.mostrarAlumno();
     
 
